7ef35d4231498698a34c639b6138f6f1f5ebd237
Kevin Olvera <kevinsolvera@gmail.com>
2024-06-02 00:01:29 -0400

    Update Contact.tsx

--- src/components/showcase/Contact.tsx
+++ src/components/showcase/Contact.tsx
@@ -63,7 +63,7 @@ const Contact: React.FC<ContactProps> = (props) => {
                 message,
             };
 
-            const result = await emailjs.send("service_fn61kt1","template_1dtj09q");
+            const result = await emailjs.send("service_fn61kt1","template_1dtj09q", templateParams);
 
             if (result.status === 200) {
                 setFormMessage(`Message successfully sent. Thank you ${name}!`);
